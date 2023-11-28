d206271deea6156c729e0d28d75d20b6b4ecc7ad
darluw <julesperrin10@gmail.com>
2023-11-28 22:48:30 +0100

    réinitialisation du dépot

--- command.js
+++
@@ -1,9 +0,0 @@
-var  Command = {
-    check: function(){
-    console.log("Cyan level checked".cyan)
-    console.log("Command Ready !")
-    console.log("Blue level checked".blue)
-    return true
-    }
-    }
-    module.exports = Command
